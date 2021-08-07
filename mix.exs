e65bfca266c5f6423b31bf6646f4efc5fb5bf6eb
Luiz Guilherme <lg.oliveira.lopes@gmail.com>
2021-08-07 12:51:06 -0300

    tech: updating elixir version to 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aluraflix.MixProject do
     [
       app: :aluraflix,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
