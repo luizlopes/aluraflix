88c5d73aa261a3885e122b307c81b5f1eeb0a486
Luiz Guilherme <lg.oliveira.lopes@gmail.com>
2021-08-11 06:05:22 -0300

    tech: create migrations to create users table

---
+++ priv/repo/migrations/20210810093939_create_users_table.exs
@@ -0,0 +1,16 @@
+defmodule Aluraflix.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :name, :string, null: false
+      add :email, :string, null: false
+      add :password, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
