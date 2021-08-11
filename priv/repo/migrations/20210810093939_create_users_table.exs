0ec6d20a1511543dae90ccb86e4832303214882f
Luiz Guilherme <lg.oliveira.lopes@gmail.com>
2021-08-11 06:21:43 -0300

    tech: fix users migrations

--- priv/repo/migrations/20210810093939_create_users_table.exs
+++ priv/repo/migrations/20210810093939_create_users_table.exs
@@ -3,7 +3,7 @@ defmodule Aluraflix.Repo.Migrations.CreateUsersTable do
 
   def change do
     create table(:users, primary_key: false) do
-      add :id, :uuid, primary_key: true
+      add :id, :uuid, primary_key: true, default: {:fragment, "uuid_generate_v1()"}
       add :name, :string, null: false
       add :email, :string, null: false
       add :password, :string, null: false
